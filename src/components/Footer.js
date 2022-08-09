f7a8f5cbd198cc5cd8730df572092f3df707cf5a
Victor Ferri <65681293+VictorFerri@users.noreply.github.com>
2022-08-08 23:58:02 -0300

    Finally One

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
-import { BackTop } from 'antd';
+import { BackTop, Input } from 'antd';
+import { Button } from "antd";
 
 class Footer extends Component {
     render() {
-        const style = {
-            
-        }
         return (
             <footer id="footer">
                 <div className="box">
-                    <div>
+                    <div className="logo-container">
                         <img src='logo-completo1.svg' />
+                        <div>
+                            <p>Receba nossas novidades</p>
+                            <div>
+                                <Input type="text" placeholder="Digite seu email" />
+                                <Button type="secondary" >Cadastrar</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='pages-container'>
                         <div className="pages">
                             <ul>
                                 <li>
@@ -39,28 +46,44 @@ class Footer extends Component {
                             </ul>
                             <div>
                                 <h3>Ajude uma entidade</h3>
-                                <p><strong>SOS Vida Animal</strong></p>
+                                <p><strong>SOS Vida Animal<img src='moreicon.svg' /></strong></p>
                                 <p>Banco Itaú: Ag <strong>8804</strong> Conta <strong>01234-5</strong></p>
-                                <p>PIX: <strong>43.660.696/0001-95</strong></p>
+                                <p>PIX: <strong>43.660.696/0001-95</strong><img src='carbon_copy-file.svg' /></p>
                             </div>
                         </div>
-                        <div className="docs">
-                            <ul>
-                                <li>Política de privacidade</li>
-                                <li>Termos de uso</li>
-                                <li>Política de Cookies</li>
-                                <li>Política de cancelamento</li>
-                            </ul>
+                        <div className="social-links">
+                            <div>
+                                <img src='facebook.svg' />
+                            </div>
+                            <div>
+                                <img src='instagram.svg' />
+                            </div>
+                            <div>
+                                <img src='youtube.svg' />
+                            </div>
                         </div>
                     </div>
-                    <div>
-
+                    <div className="docs">
+                        <ul>
+                            <li>Política de privacidade</li>
+                            <li>Termos de uso</li>
+                            <li>Política de Cookies</li>
+                            <li>Política de cancelamento</li>
+                        </ul>
                     </div>
                 </div>
                 <div id='top-button'>
-                <BackTop style={style}>
-                    <img src='topicon1.svg' />
-                </BackTop>
+                    <div>
+                        <p>
+                            Pett Love - Relacionamento | CNPJ: 43.660.696/0001-95
+                        </p>
+                    </div>
+                    <BackTop>
+                        <img src='topicon1.svg' />
+                    </BackTop>
+                    <div>
+                        <img id='logo' src='clickweblogo.svg' />
+                    </div>
                 </div>
             </footer>
         )
