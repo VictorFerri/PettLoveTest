0853defc61ec930d82e3d0c44309cd8cf35abc2d
Victor Ferri <65681293+VictorFerri@users.noreply.github.com>
2022-08-11 03:14:56 -0300

    Update Animal's Mapping

--- src/screens/PetsDating/index.js
+++ src/screens/PetsDating/index.js
@@ -1,13 +1,36 @@
-import { Row } from "antd";
+import { Button, Row } from "antd";
 import Col from "antd/es/grid/col";
 import React, { Component } from "react";
 import Footer from "../../components/Footer";
 import { motion } from "framer-motion"
 
 class PetsDating extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            isLoading: false,
+            data: [{ animal1: 'Abigail.png', animal2: 'Lisboa.png' }, { animal1: 'Guido.png', animal2: 'Chukito.png' }, { animal1: 'Lisboa.png', animal2: 'Abigail.png' }, { animal1: 'Bob.png', animal2: 'Naosei.png' }, { animal1: 'Naosei.png', animal2: 'Pindamonhangaba.png' }, { animal1: 'Bob.png', animal2: 'Naosei.png' }, { animal1: 'Abigail.png', animal2: 'Lisboa.png' }, { animal1: 'Guido.png', animal2: 'Chukito.png' }, { animal1: 'Lisboa.png', animal2: 'Abigail.png' }, { animal1: 'Bob.png', animal2: 'Naosei.png' }, { animal1: 'Naosei.png', animal2: 'Pindamonhangaba.png' }, { animal1: 'Bob.png', animal2: 'Naosei.png' }]
+        }
+    }
+
+    onLoadMore = () => {
+        this.setState({
+            isLoading: true,
+        });
+
+        setTimeout(() => {
+            this.setState({
+                data: [...this.state.data, ...this.state.data.splice(2, 3)],
+                isLoading: false,
+            });
+        }, 1500)
+    }
+
+
     render() {
 
-        const couples = [{ animal1: 'Abigail.png', animal2: 'Lisboa.png' }, { animal1: 'Guido.png', animal2: 'Chukito.png' }, { animal1: 'Lisboa.png', animal2: 'Abigail.png' }, { animal1: 'Bob.png', animal2: 'Naosei.png' }, , { animal1: 'Naosei.png', animal2: 'Pindamonhangaba.png' }, , { animal1: 'Bob.png', animal2: 'Naosei.png' }]
         return (
             <div id='petsdating'>
                 <div className='hero'>
@@ -17,40 +40,35 @@ class PetsDating extends React.Component {
                 <div className='container'>
                     <Row>
 
-                        {couples.map((c, index) => (
-                            <Col key={index} xs={24} md={12} xl={8}>
-                                <motion.div initial={{ opacity: 0 }}
-                            whileInView={{ opacity: 1 }} transition={{ delay: 0.1 }} viewport={{ once: true }}>
-                                    <div>
-                                        <img src={c.animal1} />
-                                    </div>
-                                    <div>
-                                        <img src={c.animal2} />
-                                    </div>
-                                </motion.div>
-                                <h2>{`${c.animal1.replace('.png', '')} e ${c.animal2.replace('.png', '')}`}</h2>
-                            </Col>
+                        {this.state.data.map((c, index) => (
+                            <>
+                                <Col id='col' key={index} xs={24} md={12} xl={8}>
+                                    <motion.div initial={{ opacity: 0 }}
+                                        whileInView={{ opacity: 1 }} transition={{ delay: 0.1 }} viewport={{ once: true }}>
+                                        <div>
+                                            <img src={c.animal1} />
+                                        </div>
+                                        <div>
+                                            <img src={c.animal2} />
+                                        </div>
+                                    </motion.div>
+                                    <h2>{`${c.animal1.replace('.png', '')} e ${c.animal2.replace('.png', '')}`}</h2>
+                                </Col>
+                                {index == 5 &&
+                                    (
+                                        <Col className="social" span={24}>
+                                            <div className="social-box">
+                                                <p>Publicidade 750x100</p>
+                                            </div>
+                                        </Col>
+
+                                    )}
+                            </>
                         ))}
                     </Row>
-                    <div className="social-box">
-                        <p>Publicidade 750x100</p>
+                    <div className='btn-container'>
+                        <Button onClick={this.onLoadMore} loading={this.state.isLoading} type="secondary">Carregar mais antigos</Button>
                     </div>
-                    <Row>
-                        {couples.map((c, index) => (
-                            <Col key={index} xs={24} md={12} xl={8}>
-                                <motion.div initial={{ opacity: 0 }}
-                            whileInView={{ opacity: 1 }} transition={{ delay: 0.1 }} viewport={{ once: true }}>
-                                    <div>
-                                        <img src={c.animal1} />
-                                    </div>
-                                    <div>
-                                        <img src={c.animal2} />
-                                    </div>
-                                </motion.div>
-                                <h2>{`${c.animal1.replace('.png', '')} e ${c.animal2.replace('.png', '')}`}</h2>
-                            </Col>
-                        ))}
-                    </Row>
                 </div>
                 <Footer />
             </div>
@@ -58,4 +76,4 @@ class PetsDating extends React.Component {
     }
 }
 
-export default PetsDating;
+export default PetsDating; 
